90fefb7891bb2e3034765978f6003ad6919ad98b
Vlah <brennan.vlahcevic@armssoftware.com>
2022-10-20 21:53:04 -0400

    added indexedDB API

--- client/src/js/index.js
+++ client/src/js/index.js
@@ -1,12 +1,14 @@
 import  "./form";
 import "./submit";
 import "../css/index.css";
+import {initdb} from './database';
 
 import Logo from '../images/logo.png';
 import Bear from '../images/bear.png';
 import Dog from '../images/dog.png';
 
 window.addEventListener('load', function () {
+    initdb();
     document.getElementById('logo').src = Logo;
     document.getElementById('bearThumbnail').src = Bear;
     document.getElementById('dogThumbnail').src = Dog;
